e39b7d0ed47c2b7c9973cf663326723b02698c83
jlduhaime <jldm8@hotmail.com>
2021-11-25 10:08:10 -0500

    raids point overlay

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/raidpointsoverlay/RaidsPointsConfig.java
@@ -0,0 +1,49 @@
+package net.runelite.client.plugins.raidpointsoverlay;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("raidpointsoverlay")
+public interface RaidsPointsConfig extends Config
+{
+	@ConfigItem(
+		keyName = "teamSize",
+		name = "Display team size",
+		description = "Display team size on the overlay if team size is greater than 1."
+	)
+	default boolean showTeamSize()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "raidsTimer",
+		name = "Display elapsed raid time",
+		description = "Display elapsed raid time"
+	)
+	default boolean raidsTimer()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "raidsPointsPercent",
+		name = "Display personal points percent",
+		description = "Displays the personal points percents of the total points"
+	)
+	default boolean raidsPointsPercent()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "raidsUniqueChance",
+		name = "Display the chance of an unique",
+		description = "Displays the chance that a single unique could be in raid loot"
+	)
+	default UniqueConfigOptions raidsUniqueChance()
+	{
+		return UniqueConfigOptions.BOTH;
+	}
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/raidpointsoverlay/UniqueConfigOptions.java
@@ -0,0 +1,9 @@
+package net.runelite.client.plugins.raidpointsoverlay;
+
+public enum UniqueConfigOptions
+{
+	OFF,
+	PERSONAL_CHANCE,
+	TEAM_CHANCE,
+	BOTH
+}
