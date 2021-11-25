c5d8d5b7e5363cacd59e487269901332e1f1cb7d
jlduhaime <jldm8@hotmail.com>
2021-11-25 10:11:24 -0500

    thrall helper

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/thrallhelper/ThrallHelperConfig.java
@@ -0,0 +1,39 @@
+package net.runelite.client.plugins.thrallhelper;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Units;
+
+@ConfigGroup("thrallhelper")
+public interface ThrallHelperConfig extends Config
+{
+	@ConfigItem(
+		keyName = "shouldNotify",
+		name = "Notify when thrall expires",
+		description = "Sends a notification once the thrall needs to be summoned"
+	)
+	default boolean shouldNotify()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "thrallTimeout",
+		name = "Timeout Thrall Box",
+		description = "The duration of time before the thrall box disappears."
+	)
+	@Units(Units.MINUTES)
+	default int thrallTimeout()
+	{
+		return 5;
+	}
+
+	@ConfigItem(
+		keyName = "shouldFlash",
+		name = "Flash the Reminder Box",
+		description = "Makes the reminder box flash."
+	)
+	default boolean shouldFlash() { return false; }
+
+}
