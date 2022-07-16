0a8fc8712308eedd30f7faeeee37810afaa55101
jlduhaime <14139641+jlduhaime@users.noreply.github.com>
2022-07-16 13:05:14 -0400

    update

--- runelite-client/src/main/java/net/runelite/client/RuneLiteProperties.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLiteProperties.java
@@ -125,7 +125,7 @@ public class RuneLiteProperties
 	public static HttpUrl getPluginHubBase()
 	{
 		String version = System.getProperty(PLUGINHUB_VERSION, properties.getProperty(PLUGINHUB_VERSION));
-		return HttpUrl.parse(properties.get(PLUGINHUB_BASE) + "/1.8.22");//version);
+		return HttpUrl.parse(properties.get(PLUGINHUB_BASE) + "/1.8.27");//version);
 	}
 
 	public static String getApiBase()
